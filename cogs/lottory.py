9d584f66f9ce84eab7869d6f2bc1bc8b93242d8b
Spenser Porter <Spenser.Porter@gmail.com>
2018-06-20 07:21:23 -0400

    New repo

---
+++ cogs/lottory.py
@@ -0,0 +1,307 @@
+import discord
+import random
+import numpy as np
+import lotto_dao as db
+import asyncio
+import multiprocessing
+import itertools
+from discord.ext import commands
+
+ticket_cost = 10000
+
+payout_table = {True:{0:0*ticket_cost, 1:4*ticket_cost, 2:15*ticket_cost, 3:200*ticket_cost, 4:15000*ticket_cost},
+                False:{0:0*ticket_cost, 1:0*ticket_cost, 2:0*ticket_cost, 3:20*ticket_cost, 4:1500*ticket_cost}}
+
+
+numbers = [x for x in range(1,24)]
+all_the_combs = list(itertools.combinations(numbers, 4))
+
+class Ticket(object):
+
+    __slots__ = ('numbers')
+    def __init__(self, numbers):
+        self.numbers = numbers
+
+    def __repr__(self):
+        return "{}-{}-{}-{}-M{}".format(*[n for n in self.numbers])
+
+def quickpick1(number_of_tickets=1):
+    '''Returns a number of QP tickets'''
+    qp_ticket_list = []
+    numlist = [x for x in range(1,24)]
+    for unused in range(number_of_tickets):
+        np.random.shuffle(numlist)
+        numbers = numlist[:4]
+        megaball = np.random.randint(1,12)
+        numbers.append(megaball)
+        qp_ticket_list.append(Ticket(numbers))
+    return qp_ticket_list
+
+def quickpick(number_of_tickets=1):
+    return mp_handler(number_of_tickets)
+
+def mp_handler(number_of_tickets):
+    number_of_processes = 10 if number_of_tickets >= 100000 else 1
+    avg_per = int(number_of_tickets / number_of_processes)
+    remainder = number_of_tickets % number_of_processes
+    inputs = [avg_per] * number_of_processes
+    inputs[0] += remainder
+
+    pool = multiprocessing.Pool(number_of_processes)
+    results = pool.map(quickpick1, inputs)
+    pool.close()
+    pool.join()
+    return results
+
+def db_multi_add_ticket(input):
+    ticket_list = input[0]
+    lottory_id = input[1]
+    user_id = input[2]
+    db.add_ticket_to_user(ticket_list, lottory_id, user_id)
+
+def quickpickmap(number_of_tickets=1):
+    return list(map(lambda x: list(all_the_combs[x]).append(random.randint(1,12)), [random.randint(0,len(all_the_combs)-1) for i in range(number_of_tickets)]))
+
+
+def parse_ticket(winner, ticket):
+    win_num = winner.numbers
+    tick_num = ticket.numbers
+    match = [x for x in tick_num[:4] if x in win_num[:4]]
+    mega = win_num[4] == tick_num[4]
+    return len(match), mega
+
+def add_ticket_to_winner_dict(winner_dict, user_id, ticket_value, payout):
+    if user_id not in winner_dict:
+        winner_dict[user_id] = [[ticket_value, payout]]
+    else:
+        winner_dict[user_id].append([ticket_value, payout])
+    return winner_dict
+
+def determine_payout(mega, match):
+    return payout_table[mega][match]
+
+class Lottory:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(self, message):
+        channel = message.channel
+        if message.author.id == 292953664492929025:
+            await channel.send("Shut up {}".format(message.author.nick))
+
+    @commands.group(invoke_without_command=True)
+    async def money_please(self, ctx, amount: int):
+        '''adds amount to all users balance'''
+        if ctx.author.id != 154415714411741185:
+            await ctx.send("You're not my real dad bitch!")
+            return
+        user_id_list = db.get_user()
+        for user_id in user_id_list:
+            user = await self.bot.get_user_info(user_id[0])
+            if not user.bot:
+                new_balance = db.modify_user_balance(user_id[0], amount)
+                await ctx.send('Added {} to {}\'s balance. New balance is {}'.format(amount, user.name, new_balance))
+
+    @commands.group(invoke_without_command=True)
+    async def tickets(self, ctx):
+        """
+        Show all tickets for current lottory
+        """
+        lottory = db.get_current_lottory()
+        ticket_list = db.get_user_tickets(ctx.author.id,lottory)
+        await ctx.send('{} has {} tickets in the curret drawing.'.format(ctx.author, len(ticket_list)))
+        for n in range(0, len(ticket_list), 100):
+            await ctx.author.send('{}'.format(ticket_list[n:n+100]))
+
+    @commands.group(invoke_without_command=True,aliases=['lottery'])
+    async def lottory(self,ctx):
+        if ctx.author.id != 154415714411741185:
+            await ctx.send("You're not my real dad!")
+            return
+        db.add_lottory(0)
+        lid = db.get_current_lottory()
+        await ctx.send("Lottory {} has begun, purchase tickets now!".format(lid))
+
+    @commands.group(invoke_without_command=True, aliases=['what'])
+    async def info(self, ctx):
+        await ctx.send("4 White Balls 1-23, 1 MEGABALL 1-11 - Ticket cost {:,} \n Match 4+1 win {:,} + progressive jackpot \n Match 4    win {:,} \n Match 3+1 win {:,} \n Match 3    win {:,} \n Match 2+1 win {:,}\n Match 1+1 win {:,}".format(ticket_cost, payout_table[True][4], payout_table[False][4], payout_table[True][3], payout_table[False][3], payout_table[True][2], payout_table[True][1]))
+
+
+    @lottory.command()
+    async def status(self,ctx):
+        lid = db.get_current_lottory()
+        tickets = db.get_lottory_tickets(lid)
+        num_tickets = len(tickets)
+        progressive = db.get_lottory_jackpot_prog(lid)
+        jackpot = payout_table[True][4] + progressive
+        await ctx.send("Lottory {} is in progress, currently {:,} tickets sold, current jackpot is {:,}".format(lid,num_tickets,jackpot))
+
+    @commands.group(invoke_without_command=True)
+    async def balance(self,ctx):
+        balance = db.get_user_balance(ctx.author.id)
+        lid = db.get_current_lottory()
+        ticket_list = db.get_user_tickets(ctx.author.id,lid)
+        await ctx.send("{} balance is {:,}. You have {} tickets in the next drawing".format(ctx.author.name, round(balance,2), len(ticket_list)))
+
+    @commands.group(invoke_without_command=True)
+    async def draw(self,ctx):
+        #if ctx.author.id != 154415714411741185:
+        #    await ctx.send("You're not my real dad bitch!")
+        #    return
+
+        lid = db.get_current_lottory()
+        progressive = db.get_lottory_jackpot_prog(lid)
+        total_jackpot = progressive + payout_table[True][4]
+        db.add_lottory() #increment lottory to next when drawing starts
+
+        await ctx.send("Drawing for lottory {} starting! Jackpot is currently {:,}".format(lid,total_jackpot))
+
+        winning_numbers = quickpick()[0][0] #Choose winning number_of_tickets
+
+        balls = ['First', 'Second', 'Third', 'Fourth', 'MEGA']
+        async with ctx.typing():
+            for n in range(5):
+                await asyncio.sleep(2)
+                await ctx.send("{} ball is {}".format(balls[n], winning_numbers.numbers[n]))
+            await ctx.send("Winning numbers are {}".format(winning_numbers))
+
+        ticket_list = db.get_lottory_tickets(lid) #List of tuples (user_id, ticket_value)
+        num_tickets = len(ticket_list)
+        jackpot_split = []
+        winner_dict = {}
+        total_payout = 0
+
+        async with ctx.typing():
+            for ticket_tuple in ticket_list:
+                ticket_value = Ticket(eval(ticket_tuple[0]))
+                user_id = ticket_tuple[1]
+                match, mega = parse_ticket(winning_numbers, ticket_value)
+                ticket_payout = determine_payout(mega, match)
+
+                if ticket_payout != 0:
+                    winner_dict = add_ticket_to_winner_dict(winner_dict, user_id, ticket_value, ticket_payout)
+
+        results = []
+        async with ctx.typing():
+
+            for user_id, list_of_winning_tickets in winner_dict.items():
+                balance_modifier = 0
+
+                for ticket_tuple in list_of_winning_tickets:
+                    ticket_value = ticket_tuple[0]
+                    ticket_payout = ticket_tuple[1]
+
+                    if ticket_payout == payout_table[True][4]:
+                        jackpot_split.append([user_id, ticket_value])
+                    else:
+                        balance_modifier += ticket_payout
+
+                new_user_balance = db.modify_user_balance(user_id, balance_modifier)
+                results.append([user_id, balance_modifier, new_user_balance, list_of_winning_tickets])
+                total_payout += balance_modifier
+
+            jackpot_results = []
+            if len(jackpot_split) > 0:
+                jackpot_progressive_share = round(progressive / len(jackpot_split), 2)
+                jackpot_payout = payout_table[True][4]#+ jackpot_progressive_share, 2)
+                for ticket_tuple in jackpot_split:
+                    user_id = ticket_tuple[0]
+                    ticket_value = ticket_tuple[1]
+                    total_payout += jackpot_payout
+                    new_user_balance = db.modify_user_balance(user_id, jackpot_payout)
+                    jackpot_results.append([user_id, jackpot_payout, new_user_balance, ticket_value, jackpot_progressive_share])
+
+                split_won = 'won' if len(jackpot_results) == 1 else 'split'
+                await ctx.send("------------JACKPOT WINNAR!!!!!!-------------")
+                for result in jackpot_results:
+                    user_id = result[0]
+                    jackpot_payout = result[1]
+                    new_user_balance = result[2]
+                    ticket_value = result[3]
+                    progressive_split = result[4]
+                    user = await self.bot.get_user_info(user_id)
+                    await ctx.send('{} {} the Jackpot! Payout {:,}, your share of the progressive is {:,}! with ticket {}!!!!!!!'.format(user.name, split_won, jackpot_payout, progressive_split, ticket_value))
+                    await user.send('You {} the Jackpot for lottory {} with ticket {}! {:,} has been deposited into your account. Your new balance is {}.'.format(split_won, lid, ticket_value, round(jackpot_payout,2), new_user_balance))
+
+            for result in results:
+                user_id = result[0]
+                balance_modifier=result[1]
+                new_user_balance=result[2]
+                winning_tickets=result[3]
+                user = await self.bot.get_user_info(user_id)
+                await ctx.send("{} won a total of {:,} on {:,} winning tickers!".format(user.name, balance_modifier, len(winning_tickets)))
+                await user.send("Lottory {} Results: You won {:,}. Your new balance is {:,}.".format(lid, balance_modifier, new_user_balance))
+                if len(winning_tickets) < 100:
+                    for n in range(0, len(winning_tickets), 50):
+                        await user.send("Your winnings tickets for Lottory {}: Winning Numbers:{} Your winners: {}".format(lid, winning_numbers, winning_tickets[n:n+50]))
+
+        income = ticket_cost * num_tickets
+        payout_ratio = 100 * (total_payout - income) / income
+        db.update_lottory_stats(lid, income, total_payout)
+        await ctx.send("Lottory {} ended! {:,} tickets were sold for {:,}, {:,} was paid out for a payout ratio of {}%".format(lid, num_tickets, income, round(total_payout,2), round(payout_ratio, 2)))
+        if len(jackpot_split) == 0:
+            await ctx.send("No jackpot winner!")
+            lid = db.get_current_lottory()
+            db.modify_lottory_jackpot_prog(lid, progressive)
+        else:
+            await ctx.send("Jackpot has been reseeded to {:,}".format(payout_table[True][4]))
+
+    @lottory.command(invoke_without_command=True)
+    async def stats(self, ctx, lid=None):
+        '''Returns lifetime lottory statistics, or stats of specific lottory_id if given'''
+        stats_list = db.get_lottory_stats(lid)
+        total_income = 0
+        total_outflow = 0
+        lid_total = '{}'.format(lid) if lid is not None else 'lifetime'
+        for stats_tuple in stats_list:
+            income = stats_tuple[0]
+            outflow = stats_tuple[1]
+            total_income += income
+            total_outflow += outflow
+        if total_income > 0:
+            payout_ratio = 100 * (total_outflow - total_income) / total_income
+            await ctx.send("Lottory {} stats: Total income: {:,} Total payout: {:,} Payout Ratio: {}%".format(lid_total, total_income, total_outflow, round(payout_ratio,2)))
+        else:
+            await ctx.send("There are not stats yet!")
+
+
+    @commands.group(invoke_without_command=True, aliases=['buy_tickets', 'bt'])
+    async def buy_ticket(self, ctx, ticket_value,):
+        """
+        Purchase a lottory ticket
+        """
+
+    @buy_ticket.command(aliases=['quickpick'])
+    async def qp(self, ctx, number_of_tickets=1):
+        """
+        Quick pick ticket
+        """
+        lottory_id = db.get_current_lottory()
+        user_balance = db.get_user_balance(ctx.author.id)
+        total_cost = ticket_cost * number_of_tickets
+        if user_balance < total_cost:
+            await ctx.send("That would cost {:,}, your balance is {:,}. Broke ass bitch".format(total_cost, user_balance))
+            return
+        else:
+            async with ctx.typing():
+                progressive = db.get_lottory_jackpot_prog(lottory_id)
+                ticket_lists = quickpick(number_of_tickets)
+                progressive_add = number_of_tickets * ticket_cost * .1
+                new_progressive = progressive + progressive_add
+                new_balance = db.modify_user_balance(ctx.author.id, -1 * total_cost)
+                db.modify_lottory_jackpot_prog(lottory_id, progressive_add)
+                for ticket_list in ticket_lists:
+                    db.add_ticket_to_user(ticket_list, lottory_id, ctx.author.id)
+                if len(ticket_list) <= 5:
+                    for ticket in ticket_list:
+                        await ctx.send('Quickpick ticket {} purchased by {}, good luck!'.format(ticket, ctx.author.name))
+                if number_of_tickets > 500:
+                    await ctx.author.send("You bought {} tickets. I'm not going to send you all of them.".format(number_of_tickets))
+                else:
+                    for n in range(0, len(ticket_list), 100):
+                        await ctx.author.send("Lottory {} Quickpick tickets {}".format(lottory_id, ticket_list[n:n+100]))
+                await ctx.send("{} spent {:,} on {:,} tickets, new balance is {:,}. The jackpot is now {:,}".format(ctx.author.name, total_cost, number_of_tickets, round(new_balance,2), payout_table[True][4]+new_progressive))
+
+def setup(bot):
+    bot.add_cog(Lottory(bot))
---
+++ lotto_dao.py
@@ -0,0 +1,145 @@
+import sqlite3
+
+class LottoryConnection(object):
+
+    def __init__(self):
+        self.sqlite_file = 'lottodb.sqlite'    # name of the sqlite database file
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.sqlite_file, timeout=30.0)
+        return self.connection
+
+    def __exit__(self, type, value, traceback):
+        self.connection.close()
+
+def initialize_tables():
+    table_data = {'user':{'user_id':'INTEGER PRIMARY KEY', 'balance':'REAL DEFAULT 0', 'income': 'INTEGER DEFAULT 0', 'outflow': 'INTEGER DEFAULT O'},
+                  'ticket':{'ticket_id':'INTEGER PRIMARY KEY', 'ticket_value':'TEXT', 'lottory_id':'INTEGER', 'user_id':'INTEGER'},
+                  'lottory':{'lottory_id':'INTEGER PRIMARY KEY', 'jackpot':'INTEGER DEFAULT 0', 'income': 'INTEGER DEFAULT 0', 'outflow': 'INTEGER DEFAULT 0'},
+                  }
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        for table_name, column_data in table_data.items():
+            columns = []
+            for column_name, column_type in column_data.items():
+                columns.append('{cn} {ct}'.format(cn=column_name, ct=column_type))
+            column_sql = '({})'.format(', '.join(columns))
+            sql = 'CREATE TABLE IF NOT EXISTS {tn} {ts};'.format(tn= table_name, ts=column_sql)
+            curr.execute(sql)
+        conn.commit()
+
+def add_lottory(jackpot=0):
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        sql = 'INSERT INTO lottory (jackpot) VALUES ({jp});'.format(jp=jackpot)
+        curr.execute(sql)
+        conn.commit()
+
+def get_lottory_jackpot_prog(lottory_id):
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        get_lottory = 'SELECT jackpot FROM lottory WHERE lottory_id = {id};'.format(id=lottory_id)
+        curr.execute(get_lottory)
+        lottory_jackpot = curr.fetchone()
+        return lottory_jackpot[0]
+
+def modify_lottory_jackpot_prog(lottory_id, amount):
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        get_balance_sql = 'SELECT jackpot FROM lottory WHERE lottory_id = {};'.format(lottory_id)
+        curr.execute(get_balance_sql)
+        current_balance = curr.fetchone()[0]
+        new_balance = current_balance + amount
+        set_balance_sql = 'REPLACE INTO lottory (lottory_id, jackpot) VALUES ({},{});'.format(lottory_id, new_balance)
+        curr.execute(set_balance_sql)
+        conn.commit()
+        return new_balance
+
+def update_lottory_stats(lottory_id, income, outflow):
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        sql = 'REPLACE INTO lottory (lottory_id, income, outflow) VALUES ({ld},{ic},{of});'.format(ld=lottory_id, ic=income, of=outflow)
+        curr.execute(sql)
+        conn.commit()
+
+def get_lottory_stats(lottory_id=None):
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        lottory_id_sql = '' if lottory_id is None else ' WHERE lottory_id={}'.format(lottory_id)
+        sql = 'SELECT income, outflow FROM lottory{};'.format(lottory_id_sql)
+        curr.execute(sql)
+        return curr.fetchall()
+
+
+def add_user(user_id):
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        add_user = 'INSERT OR IGNORE INTO user (user_id) VALUES ({id});'.format(id=user_id)
+        curr.execute(add_user)
+        conn.commit()
+
+def get_user(user_id=None):
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        all_users = '' if user_id is None else 'WHERE user_id ={}'.format(user_id)
+        get_user_sql = 'SELECT user_id FROM user{};'.format(all_users)
+        curr.execute(get_user_sql)
+        return curr.fetchall()
+
+
+def add_ticket_to_user(ticket_list, lottory_id, user_id):
+    with LottoryConnection() as conn:
+        curr=conn.cursor()
+        curr.execute('BEGIN TRANSACTION')
+        sql_list = []
+        for ticket in ticket_list:
+            ticket_value = str(ticket.numbers)
+            sql_list.append((ticket_value, lottory_id, user_id))
+        add_ticket_sql = 'INSERT INTO ticket (ticket_value, lottory_id, user_id) VALUES (?,?,?)'
+        curr.executemany(add_ticket_sql, sql_list)
+        conn.commit()
+
+def get_user_balance(user_id):
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        get_balance_sql = 'SELECT balance FROM user WHERE user_id = {};'.format(user_id)
+        curr.execute(get_balance_sql)
+        return curr.fetchone()[0]
+
+def modify_user_balance(user_id, amount):
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        get_balance_sql = 'SELECT balance FROM user WHERE user_id = {};'.format(user_id)
+        curr.execute(get_balance_sql)
+        current_balance = curr.fetchone()[0]
+        new_balance = current_balance + amount
+        set_balance_sql = 'REPLACE INTO user (user_id, balance) VALUES ({},{});'.format(user_id, new_balance)
+        curr.execute(set_balance_sql)
+        conn.commit()
+        return new_balance
+
+def get_current_lottory():
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        get_lotto_sql = 'SELECT * FROM lottory ORDER BY lottory_id DESC LIMIT 1;'
+        curr.execute(get_lotto_sql)
+        return curr.fetchone()[0]
+
+def get_lottory_tickets(lottory_id):
+        with LottoryConnection() as conn:
+            curr = conn.cursor()
+            get_tickets_sql = 'SELECT ticket_value, user_id FROM ticket WHERE lottory_id={};'.format(lottory_id)
+            curr.execute(get_tickets_sql)
+            return curr.fetchall()
+
+def get_user_tickets(user_id, lottory_id=None):
+    with LottoryConnection() as conn:
+        curr = conn.cursor()
+        ld_sql = ' AND lottory_id = {ld}'.format(ld=lottory_id) if lottory_id is not None else ''
+        get_ticket_sql = 'SELECT ticket_value FROM ticket WHERE user_id = {id}{ld};'.format(id=user_id, ld=ld_sql)
+        curr.execute(get_ticket_sql)
+        tickets = curr.fetchall()
+        output = []
+        for ticket in tickets:
+            output.append(ticket[0])
+        return output
